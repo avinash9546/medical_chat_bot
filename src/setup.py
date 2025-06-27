03199968feec57873fef77313e5614e1e06369fb
avinash9546 <meavinashsingh999@gmail.com>
2025-06-28 01:59:15 +0530

    folder structure added

---
+++ src/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI project',
+    version= '0.0.0',
+    author= 'Avinash Bhardwaj',
+    author_email='abibhardwaj21@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+    
+)
